bdf3578c0838e809e044efebc18de96e948cb6f4
Skybeorn <Skybeorn@10.30.16.233>
2013-02-06 16:09:17 -0600

    Small Servlet Example

---
+++ src/java/controller/JavaServlet.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Skybeorn
+ */
+@WebServlet(name = "JavaServlet", urlPatterns = {"/JavaServlet"})
+public class JavaServlet extends HttpServlet {
+
+    private static final String RESULT_PAGE = "/RectangleResult.jsp";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String number = request.getParameter("length");
+        out.print("<strong>" + number + "</strong>" + " That Text is Bold because of the Servlet");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
---
+++ web/index.jsp
@@ -0,0 +1,29 @@
+<%-- 
+    Document   : index
+    Created on : Feb 6, 2013, 3:49:54 PM
+    Author     : Skybeorn
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Servlet Example</title>
+    </head>
+    <body>
+<form id="one" name="one" method="POST" action="JavaServlet">
+            <table border="0" width="230">
+                <tr>
+                    <td align="center">
+                        Enter a #: <input type="text" name="length"> 
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center">
+                        <input type="submit" value="Calculate">
+                    </td>
+                </tr>
+            </table>
+    </body>
+</html>
